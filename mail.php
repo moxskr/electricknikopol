a4c6122d0f06e69389696a50112b755057d5a4ce
Max Skrypnyk <moxskr@gmail.com>
2020-06-21 14:07:46 +0300

    mail php

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$recepient = "moxskr@gmail.com";
+$sitename = "Название сайта";
+
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
+$text = trim($_GET["text"]);
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
